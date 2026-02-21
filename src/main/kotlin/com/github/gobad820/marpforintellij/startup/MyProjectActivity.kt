36df2fc7e8330fb2e7223623f349372bfcfb1da3
GitHub Action <action@github.com>
2026-02-21 01:37:42 +0000

    Template cleanup

--- src/main/kotlin/com/github/gobad820/marpforintellij/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/gobad820/marpforintellij/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.gobad820.marpforintellij.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
